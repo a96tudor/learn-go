f216d14c864b97452b7026dafe96ad757c8a9d19
Tudor Avram <a96tudor@gmail.com>
2023-04-03 21:44:31 +0100

    Create basic small web application

---
+++ basic-web-app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"basic-web-app/handlers"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const portNumber = ":8000"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(
+		fmt.Sprintf("Starting the application at 127.0.0.1%s", portNumber),
+	)
+	err := http.ListenAndServe(portNumber, nil)
+	log.Println(err)
+}
